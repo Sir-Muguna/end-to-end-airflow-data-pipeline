e006fdb93fb683cc00045239c6e0470d9dce91d2
Sir-Muguna <mwendasir@gmail.com>
2024-08-30 12:55:06 +0300

    update new changes

--- include/dbt/models/transform/cleaned_uber_data.sql
+++ include/dbt/models/transform/cleaned_uber_data.sql
@@ -7,8 +7,8 @@
 WITH cleaned_uber_data AS (
   SELECT
     CAST(VendorID AS INTEGER) AS VendorID,
-    CAST(tpep_pickup_datetime AS STRING) AS tpep_pickup_datetime,
-    CAST(tpep_dropoff_datetime AS STRING) AS tpep_dropoff_datetime,
+    CAST(tpep_pickup_datetime AS TIMESTAMP) AS tpep_pickup_datetime,
+    CAST(tpep_dropoff_datetime AS TIMESTAMP) AS tpep_dropoff_datetime,
     CAST(passenger_count AS INTEGER) AS passenger_count,
     CAST(trip_distance AS FLOAT64) AS trip_distance,
     CAST(pickup_longitude AS FLOAT64) AS pickup_longitude,
